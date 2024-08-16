aa102fa82edbe05b7c856165ae42e345b6460dd5
Lokee86 <otstructures@gmail.com>
2024-08-16 14:45:52 -0700

    window added and running

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    pygame.init()
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        window.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
