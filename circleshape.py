c278d43466660d484234655ac2ac51a8185caf51
Lokee86 <otstructures@gmail.com>
2024-08-16 18:33:06 -0700

    added stuff, prepping for player.

---
+++ circleshape.py
@@ -0,0 +1,17 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        super().__init__(self, self.containers)
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
--- main.py
+++ main.py
@@ -4,15 +4,20 @@ from constants import *
 def main():
     print("Starting asteroids!")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        window.fill((0, 0, 0))
+        window.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
