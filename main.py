6166b1f7aab7549caffe8a0d987054df697aba0a
Lokee86 <otstructures@gmail.com>
2024-08-17 23:32:04 -0700

    movement working

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
+        
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
         
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
@@ -17,3 +18,25 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rotate(dt * -1)
+
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(dt * -1)
