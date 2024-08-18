494662e94c057758667d220e6c6f591c58d07da9
Lokee86 <otstructures@gmail.com>
2024-08-18 13:52:39 -0700

    added sprite groups

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
-
+    
+    drawable = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2), PLAYER_RADIUS)
-
+    
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
@@ -18,10 +21,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for obj in updateable:
+            obj.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
         
         pygame.display.flip()
+
         dt = (clock.tick(60) / 1000)
         
 
