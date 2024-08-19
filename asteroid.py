d5cb55c5c4efcc4334652ccb6e94b75125fdfa6d
Lokee86 <otstructures@gmail.com>
2024-08-18 20:29:47 -0700

    asteroids splitting correctly

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 from circleshape import *
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -20,3 +22,13 @@ class Asteroid(CircleShape):
 
     def split(self):
         self.kill()
+        if self.radius == ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(5, 75)
+        new_velocity1 = self.velocity.rotate(random_angle)
+        new_velocity2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = new_velocity1 * ACCEL_FACTOR
+        asteroid2.velocity = new_velocity2 * ACCEL_FACTOR
