af387eade412040599d4d821d8e21cfc03f1a303
Lokee86 <otstructures@gmail.com>
2024-08-18 23:56:55 -0700

    GAME OVER! screen working, death sounds added

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     
     pygame.font.init()
     font = pygame.font.Font(None, 36)
+    font2 = pygame.font.Font(None, 180)
 
     drawable = pygame.sprite.Group()
     updateable = pygame.sprite.Group()
@@ -27,6 +28,8 @@ def main():
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2), PLAYER_RADIUS, "player.png")
     asteroid_field = AsteroidField()
 
+    game_over_check = False
+
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
@@ -34,6 +37,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
         for obj in updateable:
             obj.update(dt)
             
@@ -47,20 +51,39 @@ def main():
         score_rect = score_text.get_rect()
         score_rect.topright = (SCREEN_WIDTH - 10, 10)
         screen.blit(score_text, score_rect)
+    
         
+
+        if not game_over_check:
+            for asteroid in asteroids:
+                if player.collision_check(asteroid):
+                    print("Game over!")
+                    print(player.score)
+                    player.kill()
+                    ship_death.play()
+                    pygame.time.wait(1100)
+                    game_over.play()
+                    game_over_check = True
+                
+                for shot in shots:
+                    if asteroid.collision_check(shot):
+                        player.score += asteroid.split()
+                        shot.kill()
+        
+        if game_over_check:
+            game_over_msg1 = font2.render("GAME", False, "red")
+            x1 = (SCREEN_WIDTH // 2) - (game_over_msg1.get_width() // 2)
+            y1 = (SCREEN_HEIGHT // 2) - (game_over_msg1.get_height())
+            screen.blit(game_over_msg1, (x1, y1))
+
+            game_over_msg2 = font2.render("OVER!", False, "red")
+            x2 = (SCREEN_WIDTH // 2) - (game_over_msg2.get_width() // 2)
+            y2 = (SCREEN_HEIGHT // 2)
+            screen.blit(game_over_msg2, (x2, y2))
+
+
         pygame.display.flip()
-
-        for asteroid in asteroids:
-            if player.collision_check(asteroid):
-                print("Game over!")
-                print(player.score)
-                return
-            for shot in shots:
-                if asteroid.collision_check(shot):
-                    player.score += asteroid.split()
-                    shot.kill()
-
-
+        
         dt = (clock.tick(60) / 1000)
         
 
--- sound.py
+++ sound.py
@@ -7,3 +7,5 @@ pygame.mixer.init()
 
 laser_sound = pygame.mixer.Sound("laser.wav")
 rock_death = pygame.mixer.Sound("explosion.mp3")
+ship_death = pygame.mixer.Sound("ship_death.mp3")
+game_over = pygame.mixer.Sound("gameover.mp3")
